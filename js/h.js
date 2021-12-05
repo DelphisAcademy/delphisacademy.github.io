ced9287bee70c0dbd725a4ecb10530fcfc0af8fd
DelphisAcademy <89313128+DelphisAcademy@users.noreply.github.com>
2021-12-05 17:28:21 +0100

    Nivel 1(Pago) Y Nivel2

---
+++ js/h.js
@@ -0,0 +1,28 @@
+function hola() {
+    var intentar = prompt('Estamos siendo ')
+
+    if (intentar == 'We are being') {
+        alert('Perfecto')
+
+    } else {
+        alert('Mal')
+    }
+}
+
+function Adios() {
+    var intentar = prompt('Hemos sido ')
+    if (intentar == 'We have been') {
+        swal('Perfecto')
+    } else {
+        swal('Mal')
+    }
+}
+
+function SemPro() {
+    var intentar = prompt('Estabamos')
+    if (intentar == 'We were') {
+        alert('Perfecto')
+    } else {
+        alert('Mal')
+    }
+}
